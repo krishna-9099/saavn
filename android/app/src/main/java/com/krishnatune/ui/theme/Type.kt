fd1e10b18d4e1cf227384ff17d427d5523c15adf
Krishan Nishad <143264947+krishna-9099@users.noreply.github.com>
2026-04-19 13:13:32 +0530

    Add Typography file

---
+++ android/app/src/main/java/com/krishnatune/ui/theme/Type.kt
@@ -0,0 +1,23 @@
+package com.krishnatune.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    headlineMedium = TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    titleMedium = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.SemiBold
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 14.sp
+    ),
+    bodySmall = TextStyle(
+        fontSize = 12.sp
+    )
+)
