0a6a142f07b6bc2daedbda12b848a62935182a69
Krishan Nishad <143264947+krishna-9099@users.noreply.github.com>
2026-04-19 13:17:56 +0530

    Create modular HomeScreen

---
+++ android/app/src/main/java/com/krishnatune/ui/home/HomeScreen.kt
@@ -0,0 +1,47 @@
+package com.krishnatune.ui.home
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.krishnatune.domain.model.Song
+import com.krishnatune.ui.home.components.SectionRow
+import com.krishnatune.ui.home.components.TopBar
+
+@Composable
+fun HomeScreen(
+    onSongClick: () -> Unit
+) {
+
+    val mockSongs = List(10) {
+        Song(
+            it.toString(),
+            "Song $it",
+            "Artist",
+            "https://picsum.photos/200?random=$it",
+            ""
+        )
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        item {
+            TopBar()
+        }
+
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+            SectionRow(title = "Trending", songs = mockSongs, onSongClick)
+        }
+
+        item {
+            SectionRow(title = "For You", songs = mockSongs, onSongClick)
+        }
+
+        item {
+            SectionRow(title = "Recently Played", songs = mockSongs, onSongClick)
+        }
+    }
+}
