96ff1158370b9d38c3b8e311399862232c73d8dc
Krishan Nishad <143264947+krishna-9099@users.noreply.github.com>
2026-04-19 13:12:27 +0530

    Add Color theme file

---
+++ android/app/src/main/java/com/krishnatune/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.krishnatune.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenPrimary = Color(0xFF1DB954)
+val GreenDark = Color(0xFF1AA34A)
+val BlackBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val WhiteText = Color(0xFFFFFFFF)
+val GrayText = Color(0xFFB3B3B3)
